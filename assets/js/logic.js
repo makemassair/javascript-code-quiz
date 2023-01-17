d2822c57b7f71feace85afecea0bd6a5e6439af2
Michael Samaras <mike@makemassair.co.uk>
2023-01-17 13:08:52 +0000

    working: Q title. not working: questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,12 +2,13 @@ const beginQuiz = document.querySelector(".start");
 const quizIntro = document.querySelector("#start-screen");
 const question = document.querySelector("#questions");
 const questionTitle = document.querySelector("#question-title");
+const questionChoices = document.querySelector("#choice");
 
 // declared variable
 activeQuestion = [];
 
 // convert questionsToAsk object to array
-// const questionContent = Object.values(questionsToAsk);
+const questionContent = Object.values(questionsToAsk);
 // console.log(questionContent);
 
 // hide the introduction
@@ -26,7 +27,18 @@ beginQuiz.addEventListener("click", function() {
 // display the first question
 function printQ() {
     
-    // questionTitle.textContent = questionContent[0][1];
-    questionTitle.textContent = questionsToAsk.question;
-    console.log(questionTitle);
+    questionTitle.textContent = questionContent[0].question;
+    for (let i = 1; i < questionContent.length; i++) {
+        btnChoice = document.createElement("button");
+        if(questionContent[i] === "answerCorrect") {
+            btnChoice.setAttribute("data-set", "correct");
+        }
+        else {
+            btnChoice.setAttribute("data-set", "wrong");
+        };
+        btnChoice.setAttribute("class", "answerBtn");
+        document.body.children[2].children[1].children[1].appendChild(btnChoice);
+        btnChoice.textContent = i + ". " + questionContent[i].answer;
+        console.log(btnChoice);
+    }
 };
